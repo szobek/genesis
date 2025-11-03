ce3dee76e1e0ace5af75d838457b7a22f10b87f6
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-11-03 10:39:29 +0100

    Add PDF viewer component with controls and styling, update blueprint catalog page to include PDF viewer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,3 @@
 import './bootstrap';
 import './navigation.js';
+import './pdf-viewer.js';
--- resources/views/pages/blueprint-catalog.blade.php
+++ resources/views/pages/blueprint-catalog.blade.php
@@ -1,7 +1,29 @@
 <x-main-layout>
-    <x-slot name="header">
-        Címsor
-    </x-slot>
-    
-    Tartalom
+    <div class="container">
+        <x-slot name="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p>breadcrumb</p>
+                    </div>
+                </div>
+            </div>
+        </x-slot>
+        
+        <div class="row">
+            <div class="col-12">
+                <h2>
+                    Lapozza végig katalógusunkat, és fedezze fel a Genesis Házak Kft. könnyűszerkezetes épületelemeiből
+                    készült vázlatterveket.
+                </h2>
+
+                <x-pdf-viewer pdfUrl="{{ asset('storage/documents/vazlatterv-katalogus.pdf') }}" :title="'Vázlatterv katalógus'" />
+                <p>
+                    <a href="{{ asset('storage/documents/vazlatterv-katalogus.pdf') }}" class="btn btn-primary" target="_blank">Letötés</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
 </x-main-layout>
