06960c2816fa032a6e3710f6b180771da29ec9e6
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-11-03 11:38:55 +0100

    Refactor components page layout to include header and detailed content with images

--- resources/views/pages/components.blade.php
+++ resources/views/pages/components.blade.php
@@ -1,7 +1,25 @@
 <x-main-layout>
     <x-slot name="header">
-        Címsor
+        <x-header />
     </x-slot>
-    
-    Tartalom
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Rendelhető előre gyártott elemek</h1>
+                <h5>Előre gyártott elemeink teljes körű építési megoldást jelenthetnek,
+                    akár egy családi ház építéséhez is.</h5>
+            </div>
+
+            <div class="images">
+                <figure>
+                    <img src="{{ asset('storage/images/components/ablak-247x278.jpg') }} " alt="">
+                    <figcaption>Beépített ablakos falszerkezet</figcaption>
+                </figure>
+                <figure>
+                    <img src="{{ asset('storage/images/components/Kep28-247x296.png') }} " alt="">
+                    <figcaption>Beépített ajtós falszerkezet</figcaption>
+                </figure>
+            </div>
+        </div>
 </x-main-layout>
