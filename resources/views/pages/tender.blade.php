0f7b056ea26116e717f048aabbc60ee757e2fd2a
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-11-11 11:02:21 +0100

    core: add contact info to a tender page

--- resources/views/pages/tender.blade.php
+++ resources/views/pages/tender.blade.php
@@ -158,9 +158,18 @@
                 </form>
             </div>
             <div class="col-md-6">
-                <div class="d-flex flex-column text-center">
-                    <p>Telefonszám</p>
-                    <p>E-mail cím</p>
+                <div class="d-flex flex-column text-center tender-contact">
+                    <div>
+                        <img src="{{ asset('assets/images/contact/phone_icon.png') }}" alt="">
+                        <p class="title">Telefonszám</p>
+                        <p>+36 20 461 6813</p>
+                    </div>
+                    <div>
+                        
+                    </div>
+                    <img src="{{ asset('assets/images/contact/email_icon.png') }}" alt="">
+                    <p class="title">E-mail cím</p>
+                    <p>genesishaz1@gmail.com</p>
                 </div>
             </div>
         </div>
